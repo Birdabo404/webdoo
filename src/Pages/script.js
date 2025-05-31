e2bee11169e7847eed720fb9ced690a82d142dcd
Birdabo404 <birdabo420@gmail.com>
2025-06-01 02:22:57 +0800

    added function to connect javascript struct to HTML button

--- src/Pages/script.js
+++ src/Pages/script.js
@@ -2,11 +2,38 @@
 console.log("hello internet! :)");
 console.log("why are you using inspect element lol.")
 
+let allPost = [];
+//function to create post
+function createPost(author, text) {
+    const postId = Date.now();
+
+        const newPost = {
+            id: postId,
+            author: author,
+            text: text, 
+            timestamp: new Date().toLocaleDateString(),
+            likes: 0
+        };
+
+        allPost.push(newPost);
+
+        console.log("Post created:", newPost);
+        console.log("all Post:", allPost);
+}
+
 const textArea = document.getElementById('postTextArea');
-const button = document.getElementById('button');
+const submitButton = document.getElementById('button');
 
-button.addEventListener('click', function() {
-    console.log("clicked!");
-    console.log("post contains: ", textArea.value)
+submitButton.addEventListener('click', function(){
+    const postText = textArea.value;
+
+    if(postText.trim() === '') {
+        alert('Please write something.');
+        return;
+    }
+
+    createPost('Anonymous', postText);
+
+    textArea.value = '';
+    console.log('Post has been submitted!');
 });
-
